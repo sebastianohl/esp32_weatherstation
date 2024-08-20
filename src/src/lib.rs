eaee06e2f427746e04dd783f9d6b3f55bfc310a6
Sebastian Ohl <sebastian@ohl.de>
2024-08-20 19:59:49 +0200

    demo template code

---
+++ src/src/lib.rs
@@ -0,0 +1,16 @@
+// Remove if STD is supported for your platform and you plan to use it
+#![no_std]
+
+// Remove if STD is supported for your platform and you plan to use it
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
+
+//
+// Entry point
+//
+
+#[no_mangle]
+extern "C" fn app_main() {
+}

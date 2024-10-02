f1e89b50e7c25ed7a16111c8a709cc8e664cdf92
Sebastian Ohl <sebastian@ohl.de>
2024-10-02 20:31:43 +0200

    communication to as7331

--- src/src/main.rs
+++ src/src/main.rs
@@ -7,7 +7,10 @@ use esp_idf_hal::prelude::*;
 use esp_idf_hal::units::Hertz;
 
 use bme680::*;
+use core::matches;
 use core::time::Duration;
+
+use as7331_rs::*;
 
 fn i2c_master_init<'d>(
     i2c: impl Peripheral<P = impl I2c> + 'd,
@@ -45,7 +48,67 @@ fn main() {
         Err(e) => panic!("error i2c init {e:?}"),
     };
 
-    {
+    if true {
+        let lsb_a = 304.69 / ((1 << (11 - 8)) as f32) / ((1 << 9) as f32 / 1024.0) / 1000.0;
+        let lsb_b = 398.44 / ((1 << (11 - 8)) as f32) / ((1 << 9) as f32 / 1024.0) / 1000.0;
+        let lsb_c = 191.41 / ((1 << (11 - 8)) as f32) / ((1 << 9) as f32 / 1024.0) / 1000.0;
+        let mut as7331_sensor = As7331::new(i2c_master, 0x74);
+
+        log::info!("1");
+        let _ = as7331_sensor.power_up();
+        log::info!("2");
+        let _ = as7331_sensor.reset();
+        log::info!("3");
+
+        FreeRtos::delay_ms(100);
+        let chip_id = as7331_sensor.get_chip_id().unwrap();
+        log::info!("4");
+
+        if chip_id == 0x21 {
+            log::info!("state {:?}", as7331_sensor.get_mode());
+            let _ = as7331_sensor.set_configuration_mode();
+            let _ = as7331_sensor.init(
+                as7331::AS7331_CREG3_MMODE_CONT,
+                as7331::AS7331_CREG3_CCLK_1024,
+                as7331::AS7331_CREG3_SB_ON,
+                255,
+                as7331::AS7331_CREG1_GAIN_8,
+                as7331::AS7331_CREG1_TIME_512,
+            );
+            FreeRtos::delay_ms(100);
+            let _ = as7331_sensor.set_measurement_mode();
+            log::info!("state {:?}", as7331_sensor.get_mode());
+        } else {
+            panic!("Wrong chip id: {}", chip_id);
+        }
+
+        log::info!("looping");
+        loop {
+            //log::info!("state {:?}", as7331_sensor.get_mode());
+
+            let status = as7331_sensor.get_status().unwrap();
+            //log::info!("status {:?}", status);
+            if status[3] == 1 {
+                let all_data = as7331_sensor.read_all_data().unwrap();
+                let temp = all_data[0];
+                let uv_a = all_data[1];
+                let uv_b = all_data[2];
+                let uv_c = all_data[3];
+
+                log::info!("AS7331 UV DATA:");
+                log::info!("AS7331 UVA: {:.2} (uW/cm^2)", uv_a as f32 * lsb_a);
+                log::info!("AS7331 UVB: {:.2} (uW/cm^2)", uv_b as f32 * lsb_b);
+                log::info!("AS7331 UVC: {:.2} (uW/cm^2)", uv_c as f32 * lsb_c);
+                log::info!(
+                    "AS7331 Temperature: {:.2} (Celcius)",
+                    temp as f32 * 0.05 - 66.9
+                );
+            }
+            FreeRtos::delay_ms(1);
+        }
+    }
+
+    if false {
         let mut delayer = FreeRtos;
         let mut dev = match Bme680::init(i2c_master, &mut delayer, I2CAddress::Primary) {
             Err(e) => panic!("error i2c init {e:?}"),
